2b1f07c1a638d57a11ed231c335d95184896de09
Alexandar Mechev <apmechev@loui.grid.sara.nl>
2016-07-26 16:52:52 +0200

    adding proper pipeline.cfg and globus error tolerance

--- bin/getfiles.sh
+++ bin/getfiles.sh
@@ -49,7 +49,7 @@ mkfifo ${INPUT_FIFO}
 # Extract input data from input file (fifo) and catch PID
 tar -Bxf ${INPUT_FIFO} & TAR_PID=$!
 ## The untar from fifo has started, so now start download into fifo
-time globus-url-copy ${TURL_SUBBAND} file:///`pwd`/${INPUT_FIFO} && wait $TAR_PID 
+time globus-url-copy -rst -rst-retries 4 -rst-interval 1000 ${TURL_SUBBAND} file:///`pwd`/${INPUT_FIFO} && wait $TAR_PID 
 # At this point, if globus-url-copy fails it will generate a non-zero exit status. If globus-url-copy succeeds it will execute the wait for $TAR_PID, which will generate the exit status of the tar command. This means that, at this point, the exit status will only be zero when both the globus-url-copy and the tar commands finished succesfully.
 # Exit loop on non-zero e::w
 #exit status:
