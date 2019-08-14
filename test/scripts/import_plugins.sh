7a6cd9f10826825400349716ffdf005c8dbf1af9
Alexandar Mechev <apmechev@gmail.com>
2019-08-14 09:48:42 +0200

    scripts copy image into proper PWD

--- test/scripts/import_plugins.sh
+++ test/scripts/import_plugins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export RET_DIR=$PWD
+cp ../lofar.sif $RET_DIR
 cd plugins
 for  mod in *.py; do export module=${mod%.py}; echo "testing $module"; singularity exec $RET_DIR/lofar.sif python -c "from $module import *"; done
 cd $RET_DIR
